9c03289f79f8d484a63b673e9fa98b477f98d916
Diego Filastro <d.filastro@hotmail.com>
2022-12-30 19:47:11 -0300

    style: changed button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import Button from '@mui/material/Button';
 
 const ButtonContainer = styled.button`
   background-color: #24374e;
@@ -12,16 +13,34 @@ const ButtonContainer = styled.button`
   text-transform: uppercase;
   cursor: pointer;
 `;
+
 type Button = {
   type?: 'button' | 'submit' | 'reset' | undefined;
   text: string;
+  variant?: 'contained' | 'outlined';
+  background?: string;
+  color?: string;
   onClick?: () => void;
 };
 
-export default function Button(props: Button) {
+export default function ButtonTest(props: Button) {
   return (
-    <ButtonContainer type={props.type} onClick={props.onClick}>
+    <Button
+      variant={props.variant ? props.variant : 'contained'}
+      sx={{
+        backgroundColor: `${props.background}`,
+        color: `${props.color}`,
+        width: '20rem',
+        padding: '0.5rem',
+        margin: '1rem auto',
+        letterSpacing: '0.15rem',
+        textTransform: 'uppercase',
+        cursor: 'pointer',
+      }}
+      type={props.type}
+      onClick={props.onClick}
+    >
       {props.text}
-    </ButtonContainer>
+    </Button>
   );
 }
